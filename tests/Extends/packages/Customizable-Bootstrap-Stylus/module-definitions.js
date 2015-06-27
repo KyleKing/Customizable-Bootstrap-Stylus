08efc3057daa4dc389dfa8ed420b3e2d676c9e8f
Kyle King <kmking72@gmail.com>
2015-06-27 17:56:18 -0400

    Added files to test local packages

---
+++ tests/Extends/packages/Customizable-Bootstrap-Stylus/module-definitions.js
@@ -0,0 +1,75 @@
+moduleDefinitions = {
+  "utilities":{stylus:["bootstrap/bootstrap/utilities.styl"]},
+  "responsive-utilities":{stylus:["bootstrap/bootstrap/responsive-utilities.styl"]},
+
+  "variables":{stylus:["bootstrap/bootstrap/variables.styl"]},
+  "mixins":{stylus:["bootstrap/bootstrap/mixins.styl"]},
+
+  "normalize":{stylus:["bootstrap/bootstrap/normalize.styl"]},
+  "print":{stylus:["bootstrap/bootstrap/print.styl"]},
+
+  "scaffolding":{stylus:["bootstrap/bootstrap/scaffolding.styl"]},
+  "type":{stylus:["bootstrap/bootstrap/type.styl"]},
+  "code":{stylus:["bootstrap/bootstrap/code.styl"]},
+  "grid":{stylus:["bootstrap/bootstrap/grid.styl"]},
+  "tables":{stylus:["bootstrap/bootstrap/tables.styl"]},
+  "forms":{stylus:["bootstrap/bootstrap/forms.styl"]},
+  "buttons":{stylus:["bootstrap/bootstrap/buttons.styl"]},
+
+  "glyphicons":{stylus:["bootstrap/bootstrap/glyphicons.styl"]},
+  "button-groups":{stylus:["bootstrap/bootstrap/button-groups.styl"]},
+  "input-groups":{stylus:["bootstrap/bootstrap/input-groups.styl"]},
+  "navs":{stylus:["bootstrap/bootstrap/navs.styl"]},
+  "navbar":{
+    stylus:["bootstrap/bootstrap/utilities.styl","bootstrap/bootstrap/forms.styl","bootstrap/bootstrap/navs.styl","bootstrap/bootstrap/navbar.styl"],
+    js:["bootstrap/js/transition.js","bootstrap/js/collapse.js"]
+  },
+  "breadcrumbs":{stylus:["bootstrap/bootstrap/breadcrumbs.styl"]},
+  "pagination":{stylus:["bootstrap/bootstrap/pagination.styl"]},
+  "pager":{stylus:["bootstrap/bootstrap/pager.styl"]},
+  "labels":{stylus:["bootstrap/bootstrap/labels.styl"]},
+  "badges":{stylus:["bootstrap/bootstrap/badges.styl"]},
+  "jumbotron":{stylus:["bootstrap/bootstrap/jumbotron.styl"]},
+  "thumbnails":{stylus:["bootstrap/bootstrap/thumbnails.styl"]},
+  "alerts":{stylus:["bootstrap/bootstrap/alerts.styl"]},
+  "progress-bars":{stylus:["bootstrap/bootstrap/progress-bars.styl"]},
+  "media":{stylus:["bootstrap/bootstrap/media.styl"]},
+  "list-group":{stylus:["bootstrap/bootstrap/list-group.styl"]},
+  "panels":{stylus:["bootstrap/bootstrap/panels.styl"]},
+  "responsive-embed":{stylus:["bootstrap/bootstrap/responsive-embed.styl"]},
+  "wells":{stylus:["bootstrap/bootstrap/wells.styl"]},
+  "close":{stylus:["bootstrap/bootstrap/close.styl"]},
+
+  "component-animations":{
+    stylus:["bootstrap/bootstrap/component-animations.styl"],
+    js:["bootstrap/js/transition.js"]
+  },
+  "dropdowns":{
+    stylus:["bootstrap/bootstrap/dropdowns.styl"],
+    js:["bootstrap/js/dropdown.js"]
+  },
+  "modals":{
+    stylus:["bootstrap/bootstrap/modals.styl"],
+    js:["bootstrap/js/transition.js", "bootstrap/js/modal.js"]
+  },
+  "tooltip":{
+    stylus:["bootstrap/bootstrap/tooltip.styl"],
+    js:["bootstrap/js/tooltip.js"]
+  },
+  "popovers":{
+    stylus:["bootstrap/bootstrap/tooltip.styl", "bootstrap/bootstrap/popovers.styl"],
+    js:["bootstrap/js/transition.js","bootstrap/js/tooltip.js","bootstrap/js/popover.js"]
+  },
+  "carousel":{
+    stylus:["bootstrap/bootstrap/carousel.styl"],
+    js:["bootstrap/js/transition.js", "bootstrap/js/carousel.js"]
+  },
+
+  "affix":{js:["bootstrap/js/affix.js"]},
+  "alert":{js:["bootstrap/js/alert.js"]},
+  "button":{js:["bootstrap/js/button.js"]},
+  "collapse":{js:["bootstrap/js/transition.js","bootstrap/js/collapse.js"]},
+  "scrollspy":{js:["bootstrap/js/scrollspy.js"]},
+  "tab":{js:["bootstrap/js/tab.js"]},
+  "transition":{js:["bootstrap/js/transition.js"]}
+};
